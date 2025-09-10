65836b46091063c8fc5836640d66cd45df4841a1
JuCeIbMo <juceibmo@gmail.com>
2025-09-10 02:58:31 -0400

    endpoint post para enviar imagenes añadido

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 const express = require("express");
 const client = require("./main");
+const {MessageMedia} = require("whatsapp-web.js")
+
 require("dotenv").config();
 
 const app = express();
@@ -36,6 +38,27 @@ app.post("/send-message", validateToken, async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+app.post("/send-media", validateToken, async (req, res) => {
+
+  try{
+    const { channel, mediaUrl, caption } = req.body;
+    
+    if (!channel || !mediaUrl) {
+      return res.status(400).json({ error: "Faltan parámetros: number o imageUrl" });
+    }
+    
+    const media = await MessageMedia.fromUrl(mediaUrl);
+
+    await client.sendMessage(channel, media, { caption });
+
+    res.json({ status: "ok", message: "Media enviada con éxito" });
+  } catch (error){
+    console.error("Error enviando imagen:", error);
+    res.status(500).json({ error: "Error enviando imagen" });
+  }
+
 });
 
 module.exports = app;
